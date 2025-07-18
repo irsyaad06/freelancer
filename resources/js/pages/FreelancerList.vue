dc4fa8c88995874ffec266a03b9f578543134941
irsyaad06 <irsyadfatahilah@gmail.com>
2025-07-18 17:22:55 +0700

    feat : add styling on fe

--- resources/js/pages/FreelancerList.vue
+++ resources/js/pages/FreelancerList.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
-    <h2 class="text-2xl font-bold mb-4">Freelancers</h2>
-    <div class="grid md:grid-cols-3 gap-4">
+  <div class="mt-10 pt-10 px-4">
+    <h2 class="text-2xl font-bold mb-6">Freelancers</h2>
+    <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-3 gap-y-6">
       <FreelancerCard v-for="f in freelancers" :key="f.id" :freelancer="f" />
     </div>
   </div>
@@ -14,5 +14,7 @@ const freelancers = [
   { id: 1, name: 'John Doe', skill: 'Web Developer' },
   { id: 2, name: 'Jane Smith', skill: 'Graphic Designer' },
   { id: 3, name: 'Ali Akbar', skill: 'Mobile Developer' },
+  { id: 4, name: 'Duba Kuda', skill: 'Mobile kanzler' },
+  { id: 5, name: 'Aliando', skill: 'Web hack' },
 ]
 </script>
--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,12 +1,18 @@
 <script setup lang="ts">
-import FreelancerCard from '../components/FreelancerCard.vue';
-
+import FreelancerCard from "../components/FreelancerCard.vue";
+import SearchBar from "../components/SearchBar.vue";
 </script>
 
 <template>
-  <div class="text-center mt-10 pt-10">
-    <h2 class="text-3xl font-bold text-blue-600">Welcome to FreelanceFinder</h2>
-    <p class="mt-2 text-gray-700">Find the best freelancers for your project.</p>
-    <FreelancerCard />
-  </div>
+    <div class="text-center mt-10 pt-10">
+        <h2 class="text-3xl font-bold text-blue-600 mt-10">
+            Welcome to FreelanceFinder
+        </h2>
+        <p class="mt-2 text-gray-700">
+            Find the best freelancers for your project.
+        </p>
+
+        <SearchBar />
+        <FreelancerCard />
+    </div>
 </template>
