63f963402b6606e75b60c8d93699621b5108f12f
irsyaad06 <irsyadfatahilah@gmail.com>
2025-07-27 18:08:37 +0700

    hide some unused menus

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -73,7 +73,7 @@
                             Beranda
                         </router-link>
                     </li>
-                    <li>
+                    <!-- <li>
                         <a
                             href="#"
                             class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
@@ -88,7 +88,7 @@
                         >
                             Kontak
                         </a>
-                    </li>
+                    </li> -->
                 </ul>
             </div>
         </div>
