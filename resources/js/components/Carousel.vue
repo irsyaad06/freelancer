4358c74aede9ac0bf4e71b40f610ede2a0458024
irsyaad06 <irsyadfatahilah@gmail.com>
2025-08-12 10:33:28 +0700

    change frame

--- resources/js/components/Carousel.vue
+++ resources/js/components/Carousel.vue
@@ -1,34 +1,53 @@
 <template>
     <div class="relative w-full">
         <!-- Carousel wrapper -->
-        <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
-            <!-- Dynamic photo items -->
-            <div v-if="photos && photos.length > 0" class="relative h-full">
-                <div 
-                    v-for="(photo, index) in photos" 
+        <div
+            class="relative w-full bg-black overflow-hidden rounded-lg"
+            style="
+                aspect-ratio: 16/9;
+                max-width: 1920px;
+                max-height: 1080px;
+                margin: 0 auto;
+            "
+        >
+            <!-- Photo Items -->
+            <div
+                v-if="photos && photos.length > 0"
+                class="relative w-full h-full"
+            >
+                <div
+                    v-for="(photo, index) in photos"
                     :key="photo.id"
                     class="absolute inset-0 transition-opacity duration-700"
-                    :class="{ 'opacity-100': currentSlide === index, 'opacity-0': currentSlide !== index }"
+                    :class="{
+                        'opacity-100': currentSlide === index,
+                        'opacity-0': currentSlide !== index,
+                    }"
                 >
                     <img
                         :src="getProfilePhotoUrl(photo.image)"
-                        class="absolute block w-full h-full object-cover"
+                        class="w-full h-full object-cover"
                         :alt="photo.caption"
                     />
                 </div>
             </div>
-            <!-- Fallback when no photos -->
-            <div v-else class="flex items-center justify-center h-full bg-gray-200">
+
+            <!-- Fallback -->
+            <div
+                v-else
+                class="flex items-center justify-center w-full h-full bg-gray-200"
+            >
                 <img
-                    src=""
-                    class="absolute block w-full h-full object-cover"
+                    src="https://flowbite.com/docs/images/people/profile-picture-3.jpg"
+                    class="object-contain"
                     alt="Default freelancer image"
                 />
             </div>
         </div>
 
         <!-- Navigation dots -->
-        <div v-if="photos && photos.length > 1"
+        <div
+            v-if="photos && photos.length > 1"
             class="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse"
         >
             <button
@@ -46,11 +65,11 @@
         <div v-if="photos && photos.length > 1">
             <button
                 type="button"
-                class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none "
+                class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
                 @click="prevSlide"
             >
                 <span
-                    class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 group-hover:bg-white/50 group-focus:outline-none"
+                    class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 group-hover:bg-white/50"
                 >
                     <svg
                         class="w-4 h-4 text-black rtl:rotate-180"
@@ -67,7 +86,6 @@
                             d="M5 1 1 5l4 4"
                         />
                     </svg>
-                    <span class="sr-only">Previous</span>
                 </span>
             </button>
             <button
@@ -76,7 +94,7 @@
                 @click="nextSlide"
             >
                 <span
-                    class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30  group-hover:bg-white/50  group-focus:outline-none"
+                    class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 group-hover:bg-white/50"
                 >
                     <svg
                         class="w-4 h-4 text-black rtl:rotate-180"
@@ -93,20 +111,20 @@
                             d="m1 9 4-4-4-4"
                         />
                     </svg>
-                    <span class="sr-only">Next</span>
                 </span>
             </button>
         </div>
     </div>
 </template>
+
 <script setup>
-import { ref, computed } from 'vue';
+import { ref, computed } from "vue";
 
 const props = defineProps({
     photos: {
         type: Array,
         required: true,
-        default: () => []
+        default: () => [],
     },
 });
 
@@ -142,7 +160,9 @@ const nextSlide = () => {
 
 const prevSlide = () => {
     if (props.photos && props.photos.length > 0) {
-        currentSlide.value = (currentSlide.value - 1 + props.photos.length) % props.photos.length;
+        currentSlide.value =
+            (currentSlide.value - 1 + props.photos.length) %
+            props.photos.length;
     }
 };
 
